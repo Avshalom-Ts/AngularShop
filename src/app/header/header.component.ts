bc7938b44ab0dd9e92c052414351bb012dc559c1
Azdevpp <azdevpp@gmail.com>
2022-11-05 00:34:55 +0200

    LitleStyle+randomColor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,24 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() EmitshowMenu = new EventEmitter<MouseEvent>();
+  logo: string[] = ['n', 'g', 'u', 'l', 'a', 'r', ' ', 'S', 'h', 'o', 'p'];
+  letersColor: string[] = [
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+    'rgba(0,0,0,0)',
+  ];
+  r = 0;
+  g = 0;
+  b = 0;
+  a = 0;
 
   constructor() {}
 
@@ -15,5 +33,17 @@ export class HeaderComponent implements OnInit {
   amburgerToggle(e: MouseEvent) {
     this.EmitshowMenu.emit(e);
     // console.log(e);
+  }
+
+  cngColor(i: number) {
+    this.randomCol();
+    this.letersColor[i] = `rgba(${this.r},${this.g},${this.b},1)`;
+  }
+
+  randomCol() {
+    this.r = Math.floor(Math.random() * 255);
+    this.g = Math.floor(Math.random() * 255);
+    this.b = Math.floor(Math.random() * 255);
+    this.a = 1;
   }
 }
