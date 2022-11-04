eb8ef3dabc13fd141f707f8753f88c8af393ffc7
Azdevpp <azdevpp@gmail.com>
2022-11-04 10:24:38 +0200

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeroImageComponent } from './hero-image/hero-image.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductListComponent } from './ProductPage/product-list/product-list.component';
+import { ProductComponent } from './ProductPage/productList/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    HeroImageComponent,
+    ProductPageComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
